36f658847eeda54855c8dfb2618f0f28a86d0dc8
Horace Crump <hcrump@gmail.com>
2019-05-01 03:38:03 -0700

    added /media/ static folder

--- mytestsite1/settings.py
+++ mytestsite1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pk0t4=p!pmf8cr#2yda_anx0r9(w2msou$cgo&8@m1df%(5ef3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost',spingadus.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
